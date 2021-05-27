1740d1ea877d547e8da953183eaa7b4f4ded3dbf
gdhar92 <gdhar@chicagobooth.edu>
2021-05-27 05:07:40 +0000

    1st commit

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  email           :string
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  
+  validates(:username,
+    {
+      :presence => true,
+      :uniqueness => { :case_sensitive => false }
+    }
+  )
+
+  has_secure_password
+end
