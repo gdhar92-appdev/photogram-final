988204e3ff3d52bb0b3a0b255d9423217b1228da
gdhar92 <gdhar@chicagobooth.edu>
2021-05-27 09:38:01 +0000

    8th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,31 +26,55 @@ class User < ApplicationRecord
   def is_followed_by(sender_id)
     #sender_id = session.fetch(:user_id).to_i
     recipient_id = self.id
-    follow_status = FollowRequest.where({:recipient_id => recipient_id }).where({ :sender_id => sender_id }).first
-    if follow_status == "accepted"
-      return 1
-    elsif follow_status == "pending"
-      return 2
-    elsif follow_status == "rejected"
-      return 3
-    else
+    follow_request_entry = FollowRequest.where({:recipient_id => recipient_id }).where({ :sender_id => sender_id }).first
+    if follow_request_entry == nil
       return 4
+    else     
+      follow_status = follow_request_entry.status
+      if follow_status == "accepted"
+        return 1
+      elsif follow_status == "pending"
+        return 2
+      elsif follow_status == "rejected"
+        return 3
+      else
+        return 4
+      end
     end
   end
 
   def is_follower_of(recipient_id)
     sender_id = self.id 
     #recipient_id = session.fetch(:user_id).to_i
-    follow_status = FollowRequest.where({:recipient_id => recipient_id }).where({ :sender_id => sender_id }).first
-    if follow_status == "accepted"
-      return 1
-    elsif follow_status == "pending"
-      return 2
-    elsif follow_status == "rejected"
-      return 3
-    else
+    follow_request_entry = FollowRequest.where({:recipient_id => recipient_id }).where({ :sender_id => sender_id }).first
+    if follow_request_entry == nil
       return 4
+    else     
+      follow_status = follow_request_entry.status
+      if follow_status == "accepted"
+        return 1
+      elsif follow_status == "pending"
+        return 2
+      elsif follow_status == "rejected"
+        return 3
+      else
+        return 4
+      end
     end
+  end
+
+  def followers
+    user_id = self.id
+    accepted_requests = FollowRequest.where({ :status => "accepted" })
+    user_followers = accepted_requests.where({ :recipient_id => user_id})
+    return user_followers
+  end
+
+  def following
+    user_id = self.id
+    accepted_requests = FollowRequest.where({ :status => "accepted" })
+    user_following = accepted_requests.where({ :sender_id => user_id})
+    return user_following
   end
 
 end
